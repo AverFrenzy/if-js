53bd40974f81a60f43ccc79f12662b40c85d7c6c
AverFrenzy <metallfrenzy@gmail.com>
2021-09-21 14:48:17 +0300

    add condition that checks type of compared items,add condition that checks if key includes array

--- lesson-9 obj/main.js
+++ lesson-9 obj/main.js
@@ -1,5 +1,5 @@
 const obj1 = {
-  a: 'a',
+  a: ['a', 121],
   b: {
     a: 'a',
     b: 'b',
@@ -16,7 +16,7 @@ const obj2 = {
     b: 'b',
     a: 'a',
   },
-  a: 'a',
+  a: ['a', 121],
 };
 const obj3 = {
   a: {
@@ -30,17 +30,25 @@ const obj3 = {
 };
 
 const deepEqual = (obj1, obj2) => {
+  if (Array.isArray(obj1) && Array.isArray(obj2)) {
+    for (let i = 0; i < obj1.length; i++) {
+      if (obj1.length !== obj2.length || obj1[i] !== obj2[i]) {
+        return false
+      }
+    }
+  }
   if (obj1 === obj2) {
     return true;
   }
+  if (obj1 == null || typeof obj1 !== "object" || obj2 == null || typeof obj2 !== "object") {
+    return false
+  }
   let keys1 = Object.keys(obj1)
   let keys2 = Object.keys(obj2)
-
   for (let key of keys1) {
-    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
-      return false;
-    }
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) return false;
   }
+
   return true;
 }
 console.log(deepEqual(obj1, obj2)); // true
