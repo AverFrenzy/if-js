c03c88a4ea97fe25be93c8bf4c8a0884d64d6fb1
AverFrenzy <71224260+AverFrenzy@users.noreply.github.com>
2021-09-24 12:54:46 +0300

    lesson-12 DOM made 'Homes guest loves' block with JS (#10)
    * made 'Homes guest loves' block with JS
    
    * now element are created with innerHTML
    
    * now map method is used to create homesItems

---
+++ lesson-12 DOM/main.js
@@ -0,0 +1,76 @@
+const data = [
+  {
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
+  },
+  {
+    name: 'Apartment Sunshine',
+    city: 'Santa  Cruz de Tenerife',
+    country: 'Spain',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
+  },
+  {
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
+  },
+  {
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
+  },
+  {
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
+  },
+  {
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
+  },
+  {
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
+  },
+];
+
+const homesItem = data.map(elem => {
+  return (
+    `
+    <div class="homes__item">
+      <div class="homes__img-wrapper">
+        <img src="${elem.imageUrl}">
+      </div>
+        <span class="homes__aparts-name">${elem.name}</span>
+        <span class="homes__aparts-location">${elem.city}, ${elem.country}</span>
+    </div>
+    `)
+}).join('')
+
+const mainContainer = document.createElement('div')
+mainContainer.className = 'container'
+mainContainer.innerHTML =
+`
+  <div class="homes">
+    <h2 class="homes__heading">Homes guests loves</h2>
+    <div class="homes__img-container">
+      ${homesItem}
+    </div>
+  </div>
+` 
+document.body.appendChild(mainContainer)
